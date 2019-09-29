83545be047885059ee4f2f5565260e9b83ceb816
Jason Sullenger <jssullenger@my365.bellevue.edu>
2019-09-28 18:11:37 -0600

    styling completed

--- sullenger-Nodequiz/src/app/components/not-found/not-found.component.ts
+++ sullenger-Nodequiz/src/app/components/not-found/not-found.component.ts
@@ -8,18 +8,60 @@
 */
 
 import { Component, OnInit } from "@angular/core";
+import { CookieService } from "ngx-cookie-service";
+import { Router } from "@angular/router";
 
 @Component({
   selector: "app-not-found",
   template: `
-    <p>
-      not-found works!
-    </p>
+    <div class="container">
+      <mat-card color="accent" class="form">
+        <mat-card-content class="contentGroup">
+          <h1 class="contentStyle">Error 404</h1>
+          <h3 class="contentStyle">Page not found</h3>
+          <button class="contentStyle" (click)="navHome()" mat-raised-button color="primary">
+            Home
+          </button>
+        </mat-card-content>
+      </mat-card>
+    </div>
   `,
-  styles: []
+  styles: [
+    `
+      .container {
+        margin-top: 100px;
+        font-family: Arial, Helvetica, sans-serif
+      }
+
+      .form {
+        width: 60%;
+        height: 200px;
+        margin: 0 auto;
+        text-align: center;
+        background-color: grey;
+      }
+      .contentGroup {
+        padding-top: 25px;
+      }
+      .contentStyle {
+        display: block;
+        width: 50%;
+        margin: 20px auto;
+      }
+    `
+  ]
 })
 export class NotFoundComponent implements OnInit {
-  constructor() {}
+  constructor(private router: Router, private cookie: CookieService) {}
 
   ngOnInit() {}
+
+  navHome() {
+    const value: string = this.cookie.get("isAuthenticated");
+    if (value) {
+      this.router.navigate(["/home"]);
+    } else {
+      this.router.navigate(["/session/login"]);
+    }
+  }
 }
