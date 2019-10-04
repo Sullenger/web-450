30d0c93296e924004110f6bb24286578e7cfdeb7
Jason Sullenger <jssullenger@my365.bellevue.edu>
2019-10-04 13:44:49 -0600

    take quiz button routes to quiz page and correct quiz is retrieved.

--- sullenger-Nodequiz/src/app/components/quiz/quiz.component.ts
+++ sullenger-Nodequiz/src/app/components/quiz/quiz.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from "@angular/router";
+import { HttpClient } from "@angular/common/http";
 
 @Component({
   selector: 'app-quiz',
@@ -6,10 +8,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quiz.component.css']
 })
 export class QuizComponent implements OnInit {
+  quizSelection: number;
+  errorMessage: string;
 
-  constructor() { }
+  constructor(private http: HttpClient, private route: ActivatedRoute) { }
 
   ngOnInit() {
-  }
+    this.quizSelection = parseInt(this.route.snapshot.paramMap.get("id"), 10);
 
+    this.http.get("/api/quiz_bank/" + this.quizSelection).subscribe(res => {
+      if (res) {
+        console.log(res)
+      } else {
+        this.errorMessage = "We encountered an error retrieving your quiz";
+      }
+    });
+  }
 }
