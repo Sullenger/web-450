312f8f8ab42db84d0a76e5adcbed9ffc2c24ef0e
Jason Sullenger <jssullenger@my365.bellevue.edu>
2019-10-07 23:18:04 -0600

    adds some quiz functionality

--- sullenger-Nodequiz/src/app/components/quiz/quiz.component.ts
+++ sullenger-Nodequiz/src/app/components/quiz/quiz.component.ts
@@ -10,6 +10,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from "@angular/router";
 import { HttpClient } from "@angular/common/http";
+import {FormControl, FormGroup} from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
 @Component({
   selector: 'app-quiz',
@@ -20,6 +22,22 @@ export class QuizComponent implements OnInit {
   quizSelection: number;
   errorMessage: string;
   quizBody: any;
+  score: number;
+
+  public order = {
+    question1: { correctAnswer: "", submittedAnswer: "" },
+    question2: { correctAnswer: "", submittedAnswer: "" },
+    question3: { correctAnswer: "", submittedAnswer: "" },
+    question4: { correctAnswer: "", submittedAnswer: "" },
+    question5: { correctAnswer: "", submittedAnswer: "" },
+    question6: { correctAnswer: "", submittedAnswer: "" },
+    question7: { correctAnswer: "", submittedAnswer: "" },
+    question8: { correctAnswer: "", submittedAnswer: "" },
+    question9: { correctAnswer: "", submittedAnswer: "" },
+    question10: { correctAnswer: "", submittedAnswer: "" },
+  };
+
+
 
   constructor(private http: HttpClient, private route: ActivatedRoute) { }
 
@@ -30,9 +48,27 @@ export class QuizComponent implements OnInit {
       if (res) {
         this.quizBody = res;
         console.log(this.quizBody);
+        // console.log(this.quizBody.quiz_Questions[0]);
+        // for(let i=0; i<10; i++){
+        //   let iteration = "question"+(i+1);
+        //   this.order[iteration].submittedAnswer = this.quizBody.quiz_Questions[i].quiz_answers.correct_Answer;
+        //   console.log(this.order[iteration].submittedAnswer)
+        // }
       } else {
         this.errorMessage = "We encountered an error retrieving your quiz";
       }
     });
+  }
+  onSubmit(formData) {
+    console.log(formData)
+    if(formData){
+      for(let i=1; i<11; i++){
+        let iteration = "question"+i;
+        this.order[iteration].submittedAnswer = formData[iteration].submittedAnswer;
+        if(this.order[iteration].submittedAnswer === this.order[iteration].correctAnswer){
+          this.score += 1;
+        }
+      }
+    }
   }
 }
